ce7eb5d32f3a49ff819a71fdcac8008871cf9109
Ian MacLeod <ian.a.macleod@gmail.com>
2017-04-19 13:57:00 -0700

    Testing postgres

--- index.js
+++ index.js
@@ -2,12 +2,28 @@ const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const path = require('path')
+const pg = require('pg')
 
 const PORT = process.env.PORT || 3000;
 const INDEX = path.join(__dirname, 'index.html')
 
 app.get('/', function(req, res){
   res.sendFile(INDEX);
+});
+
+app.get('/db', function(req, res){
+  pg.connect(process.env.DATABASE_URL, function(err, client, done){
+    client.query('SELECT * FROM test_table', function(err, result){
+      done();
+      if (err) {
+        console.error(err);
+        res.send("error " + err);
+      } else{
+        res.setHeader('Content-Type', 'application/json');
+        res.send({results.rows});
+      }
+    });
+  });
 });
 
 http.listen(PORT, function(){
